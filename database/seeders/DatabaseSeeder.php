a033007985780f6c1f05986e357078724d5bce49
christiancamilleri <camilleri.christian@outlook.com>
2023-06-13 16:20:39 +0200

    feat: creato seeder utenti

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            UserSeeder::class,
             RestaurantSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Faker\Generator as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+
+            $user->name = $faker->firstName();
+            $user->email = $faker->email();
+            $user->password = Hash::make('password');
+
+            $user->save();
+        }
+    }
+}
