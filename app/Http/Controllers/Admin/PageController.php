276360d1f1bfa2591680880056bbe3b3875ab0af
christianarca27 <christian.arca.mail@gmail.com>
2023-06-14 14:28:04 +0200

    feat: mostrato in dashboard nome utente, nome ristorante e menu di chi ha eseguito l'accesso

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PageController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $user_id = Auth::id();
+
+        $restaurant = Restaurant::where('user_id', $user_id)->first();
+
+        if ($restaurant) {
+            return view('admin.dashboard', compact('restaurant'));
+        } else {
+            return redirect()->route('admin.restaurants.create');
+        }
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,5 +3,14 @@
 @section('content')
     <div class="container">
         <h1>Dashboard</h1>
+
+        <h2>{{ $restaurant->name }} di {{ $restaurant->user->name }}</h2>
+
+        <h3>Menu</h3>
+        <ul>
+            @foreach ($restaurant->products as $product)
+                <li>{{ $product->name }} - € {{ $product->price }}</li>
+            @endforeach
+        </ul>
     </div>
 @endsection
