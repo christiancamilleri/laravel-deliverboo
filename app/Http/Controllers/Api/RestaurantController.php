fb84fc58c0c758868b1ba531e54d140fd65b21b2
christiancamilleri <camilleri.christian@outlook.com>
2023-06-19 11:32:44 +0200

    feat: implementazione API restaurants.index

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use App\Models\Typology;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index(Request $request)
+    {
+        $requestData = $request->all();
+
+        if ($request->has('typology_id') && $requestData['typology_id']) {
+            $typology = Typology::where('id', $requestData['typology_id'])->first();
+            $restaurants = $typology->restaurants;
+        } else {
+            $restaurants = Restaurant::with('typologies')->get();
+        }
+
+        $typologies = Typology::all();
+
+        if (count($restaurants)) {
+            return response()->json([
+                'success' => true,
+                'results' => $restaurants,
+                'allTypologies' => $typologies
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun ristorante trovato',
+                'allTypologies' => $typologies,
+            ]);
+        }
+    }
+}
