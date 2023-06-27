03932cda59086390549626e4fd7157372edc252e
christianarca27 <christian.arca.mail@gmail.com>
2023-06-27 10:46:40 +0200

    feat: aggiunto file di customizzazione variabili Bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-import './bootstrap';
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
+import '../scss/custom.scss'
 import '@fortawesome/fontawesome-free/css/all.css';
 import.meta.glob([
     '../img/**'
