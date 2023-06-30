a3f9760871d0b5bfec9336eb8de97813edc5499e
danilopiccinni <danilopiccinni@gmail.com>
2023-06-30 07:26:15 +0200

    modificto i titoli nella schermata appena si logga nel caso non ci fosse ancora il risotrante

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -4,10 +4,13 @@
     <div class="container-fluid p-5 mb-5 bg-dark rounded-3 text-center ">
         <h1 class="">Benvenuto {{ $userName }}</h1>
 
+        @if($restaurant)
         <h2 class="my-5 d-none d-lg-block"><i class="fa-solid fa-circle-arrow-left"></i> Usa il menu sulla sinistra per navigare nel gestionale del tuo ristorante</h2>
         <h2 class="my-5 d-lg-none"><i class="fa-solid fa-circle-arrow-up"></i> Usa il menu in alto per navigare nel gestionale del tuo ristorante</h2>
-
-
+        @else
+        <h2 class="my-5 d-none d-lg-block"><i class="fa-solid fa-circle-arrow-left"></i>Usa il pulsante sulla sinistra 'Crea il tuo ristorante' per iniziare</h2>
+        <h2 class="my-5 d-lg-none"><i class="fa-solid fa-circle-arrow-up"></i>Usa il pulsante in alto 'Crea il tuo ristorante' per iniziare</h2>
+        @endif
         <div class="d-flex flex-column align-items-center bg-success rounded-5 p-5">
             
             <h2>Necessiti di supporto?</h2>
